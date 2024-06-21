c73d02ed38cbcc31e2a419c3e49729b2bcc2be62
Jer-Shen Chen <jschen1217@gmail.com>
2024-06-21 14:32:43 -0400

    update

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          BASE_URL: process.env.NODE_ENV === 'production' ? '/testweb/' : '/',
+        },
+      },
+    }),
+  ],
   build: {
     outDir: 'dist',
   },
