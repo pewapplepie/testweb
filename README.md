fc7cbb332aa4189b973f252c169889e395cf9d66
Jer-Shen Chen <jschen1217@gmail.com>
2024-06-21 13:46:52 -0400

    adding web; ;

---
+++ README.md
@@ -0,0 +1,12 @@
+
+  # Personal portfolio
+
+  redesign the website also framework feel free to use
+
+  Note: Please ensure you have installed <code><a href="https://nodejs.org/en/download/">nodejs</a></code>
+
+  To preview and run the project on your device:
+  1) Open project folder in <a href="https://code.visualstudio.com/download">Visual Studio Code</a>
+  2) In the terminal, run `npm install`
+  3) Run `npm start` to view project in browser
+  
---
+++ vite.config.mjs
@@ -0,0 +1,32 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   build: {
+//     outDir: "build",
+//   },
+//   plugins: [react()],
+//   base: process.env.NODE_ENV === 'production' ? '/your-repository-name/' : '/', // Only set base for production
+// });
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          BASE_URL: process.env.NODE_ENV === 'production' ? '/jeffreychen.ha/' : '/',
+        },
+      },
+    }),
+  ],
+  build: {
+    outDir: 'build',
+  },
+  base: process.env.NODE_ENV === 'production' ? '/jeffreychen.ha/' : '/',
+});
